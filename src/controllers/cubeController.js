f498ec3d83fab0245a4dc9c9bd408dd50c15680e
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-01-18 21:43:15 +0200

    Add search

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -19,13 +19,13 @@ exports.getDetails = (req, res) => {
     let cubeId = Number(req.params.cubeId);
 
     if (!cubeId) {
-        return res.redirect('404');
+        return res.redirect('/404');
     }
 
     let cube = db.cubes.find(x => x.id === cubeId);
 
     if (!cube) {
-        return res.redirect('404');
+        return res.redirect('/404');
     }
 
     res.render('details', { cube });
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const homeController = require('./controllers/homeController');
 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
+router.get('/404', homeController.getErrorPage);
 
 router.get('/create', cubeController.getCreateCube);
 router.post('/create', cubeController.postCreateCube);
