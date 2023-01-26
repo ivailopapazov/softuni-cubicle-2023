2f31f72010db1704f7d2cfcb962da72646d50496
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-01-26 19:33:56 +0200

    Show cube details from db

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,5 +1,4 @@
 const Cube = require('../models/Cube');
-const db = require('../db.json');
 
 exports.getCreateCube = (req, res) => {
     res.render('create');
@@ -15,14 +14,8 @@ exports.postCreateCube = async (req, res) => {
     res.redirect('/');
 };
 
-exports.getDetails = (req, res) => {
-    let cubeId = Number(req.params.cubeId);
-
-    if (!cubeId) {
-        return res.redirect('/404');
-    }
-
-    let cube = db.cubes.find(x => x.id === cubeId);
+exports.getDetails = async (req, res) => {
+    const cube = await Cube.findById(req.params.cubeId).lean();
 
     if (!cube) {
         return res.redirect('/404');
