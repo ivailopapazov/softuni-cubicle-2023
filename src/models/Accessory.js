461f15f27ed6e864ed2793bcf9043827079a875b
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-01-26 20:02:19 +0200

    Add accessory model

---
+++ src/models/Accessory.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const accessorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 50
+    },
+});
+
+const Accessory = mongoose.model('Accessory', accessorySchema);
+
+module.exports = Accessory;
