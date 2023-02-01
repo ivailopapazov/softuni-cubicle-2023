107908b5fd17dddfb49468b7f495a3f1c0732808
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-02-01 20:48:21 +0200

    Add authorization in one endpoint

--- src/lib/jsonwebtoken.js
+++ src/lib/jsonwebtoken.js
@@ -1,6 +1,20 @@
 const util = require('util');
 const jwtCallback = require('jsonwebtoken');
 
+// function promiseSign(payload, secret, options) {
+//     const promise = new Promise(function(resolve, reject) {
+        // jwtCallback.sign(payload, secret, options, (err, token) => {
+        //     if (err) {
+        //         return reject(err);
+        //     }
+
+        //     resolve(token);
+        // });
+//     });
+
+//     return promise;
+// }
+
 const jwt = {
     sign: util.promisify(jwtCallback.sign),
     verify: util.promisify(jwtCallback.verify),
