b5557e633305255f167e4ff3b9c629af0ad7c372
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-02-01 18:32:07 +0200

    Add login and register pages

---
+++ src/controllers/authController.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+
+    if (password !== repeatPassword) {
+        return res.status(404).end();
+    }
+});
+
+module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,13 @@ const router = require('express').Router();
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
 const accessoryController = require('./controllers/accessoryController');
+const authController = require('./controllers/authController');
 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
 router.get('/404', homeController.getErrorPage);
+
+router.use('/', authController);
 
 router.get('/cubes/create', cubeController.getCreateCube);
 router.post('/cubes/create', cubeController.postCreateCube);
